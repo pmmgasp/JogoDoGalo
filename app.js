703d96cb0f2db238db32e7719e7e6af472ef22d9
Pedro Gaspar <aluno23038@ipt.pt>
2023-02-04 18:43:02 +0000

    Updt api

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express');
-const routeUtils = require('express-recursive-routes');
+const cors = require('cors');
+const authRouter = require("./routes/auth");
+const usersRouter = require("./routes/users");
+const healthRouter = require("./routes/health");
 
 // express app
 const app = express();
 
 // configure express to use json as middle-ware
+app.use(cors());
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Method", "POST, GET, OPTIONS");
-  res.header("Access-Control-Allow-Methods", "PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Allow", "*");
-  next();
-});
 
-routeUtils.mountRoutes(app, "./routes/");
+app.use("/auth", authRouter);
+app.use("/users", usersRouter);
+app.use("/health", healthRouter);
 
 // listen for requests
 const PORT = process.env.PORT || 5000;
